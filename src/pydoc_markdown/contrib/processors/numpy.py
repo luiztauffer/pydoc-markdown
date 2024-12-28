4919c1b6b86fc017b74e064b2b76d44fb3fd4c69
luiz <luiz_tauffer@hotmail.com>
2024-12-28 11:24:58 +0100

    remove print

--- src/pydoc_markdown/contrib/processors/numpy.py
+++ src/pydoc_markdown/contrib/processors/numpy.py
@@ -159,7 +159,6 @@ class NumpyProcessor(Processor):
             "Summary",
             *[next(key for key, value in active_sections.items() if keyword in value) for keyword in keyword_matches],
         ]
-        print(section_order)
 
         # Finally, we sort active_sections according to the section order we just determined
         for section, keywords in sorted(active_sections.items(), key=lambda x: section_order.index(x[0])):
